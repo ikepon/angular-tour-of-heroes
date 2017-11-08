ea37e88ad0a55083c7ffebc0cfdf6f627c60fe14
ikepon <tatsuyanoheya@gmail.com>
2017-11-08 11:46:46 +0900

    Hero name Update

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -28,6 +28,11 @@ export class HeroDetailComponent implements OnInit {
       .subscribe(hero => this.hero = hero);
   }
 
+  save(): voide {
+    this.heroService.update(this.hero)
+      .then(() => this.goBack());
+  }
+
   goBack(): void {
     this.location.back();
   }
